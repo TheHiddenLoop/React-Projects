dab29b699242a1876389a58d2fe4937a7ed7dfd9
Angesh0007 <chauhanangesh60@gmail.com>
2025-07-06 17:52:12 +0530

    New Project

---
+++ calculator/src/App.jsx
@@ -0,0 +1,11 @@
+import { Calculator } from "./components/Calculator";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="calculator-container">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
