b0100788938f27db7df0cfa4ead63eb91db635ee
Angesh0007 <chauhanangesh60@gmail.com>
2025-07-13 16:42:17 +0530

    New Project

---
+++ slider/src/App.jsx
@@ -0,0 +1,12 @@
+import Slider from "./components/Slider"
+function App() {
+  return (
+    <div className="min-h-screen pt-10 w-full bg-slate-600">
+      <div className="max-w-[1200px] mx-auto">
+        <Slider />
+      </div>
+    </div>
+  );
+}
+
+export default App;
